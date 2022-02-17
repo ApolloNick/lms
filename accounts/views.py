1573aca8dc06f11913a0eefdc2d1f30e128c5d68
ApolloNick <nkostrov7@gmail.com>
2022-02-17 10:15:26 +0200

    Password change

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,8 @@
-from django.contrib.auth import update_session_auth_hash
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView, PasswordChangeView
 from django.urls import reverse_lazy
-from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_protect
-from django.views.decorators.debug import sensitive_post_parameters
 from django.views.generic import CreateView, UpdateView
 from accounts.forms import AccountRegisterForm, AccountProfileForm
 
@@ -35,10 +30,5 @@ class AccountEdit(LoginRequiredMixin, UpdateView):
 
 class AccountChangePassword(LoginRequiredMixin, PasswordChangeView):
     form_class = PasswordChangeForm
-    success_url = reverse_lazy("password_change_done")
+    success_url = reverse_lazy('accounts:password_change_done')
     template_name = "accounts/password_change.html"
-
-    def form_valid(self, form):
-        form.save()
-        update_session_auth_hash(self.request, form.user)
-        return super().form_valid(form)
